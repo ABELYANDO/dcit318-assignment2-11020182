1aad70a8a3b8436876f8d3f207265c9d7aa0ead1
ABEL KWEKU YANDO <yandoabel1998@gmail.com>
2025-08-02 23:33:33 +0000

    Add files via upload

---
+++ Animal.cs/Animal.cs/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Animal
+{
+    public virtual void MakeSound()
+    {
+        Console.WriteLine("Some generic sound");
+    }
+}
+
+class Dog : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Bark");
+    }
+}
+
+class Cat : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Meow");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Animal genericAnimal = new Animal();
+        Animal dog = new Dog();
+        Animal cat = new Cat();
+
+        genericAnimal.MakeSound(); // Some generic sound
+        dog.MakeSound();           // Bark
+        cat.MakeSound();           // Meow
+    }
+}
+
---
+++ Interfaces.cs/Interfaces.cs/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+interface IMovable
+{
+    void Move();
+}
+
+class Car : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("Car is moving");
+    }
+}
+
+class Bicycle : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("Bicycle is moving");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        IMovable car = new Car();
+        IMovable bicycle = new Bicycle();
+
+        car.Move();      // Car is moving
+        bicycle.Move();  // Bicycle is moving
+    }
+}
+
